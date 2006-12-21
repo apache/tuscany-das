39d8b9205bf4590ecad77afc77418e698fa5a6da
Luciano Resende <lresende@apache.org>
2006-12-21 15:51:58 +0000

    TUSCANY-1008
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/das@489391 13f79535-47bb-0310-9956-ffa450edef68

--- rdb/src/test/java/org/apache/tuscany/das/rdb/test/PartialUpdateTests.java
+++ rdb/src/test/java/org/apache/tuscany/das/rdb/test/PartialUpdateTests.java
@@ -42,7 +42,10 @@ public class PartialUpdateTests extends DasTest {
         super.tearDown();
     }
 
-    public void testPartialUpdate() throws SQLException {
+    /**
+     * Update with one changed property
+     */
+    public void testPartialUpdateSingleProperty() throws SQLException {
         DAS das = DAS.FACTORY.createDAS(getConnection());
         Command readCustomers = das.createCommand("select * from CUSTOMER where ID = 1");
 
@@ -69,9 +72,172 @@ public class PartialUpdateTests extends DasTest {
         assertEquals("main street", customer.getString("ADDRESS"));
     }
 
-    public void testPartialInsert() throws SQLException {
+    /**
+     * Update with all changed properties
+     */
+    public void testPartialUpdateAllProperties() throws SQLException {
         DAS das = DAS.FACTORY.createDAS(getConnection());
         Command readCustomers = das.createCommand("select * from CUSTOMER where ID = 1");
+
+        // Read
+        DataObject root = readCustomers.executeQuery();
+
+        DataObject customer = root.getDataObject("CUSTOMER[1]");
+        // Verify
+        assertEquals(1, customer.getInt("ID"));
+
+        customer.setString("LASTNAME", "lastname modified");
+        customer.setString("ADDRESS", "address modified");
+        
+        das.applyChanges(root);
+
+        root = readCustomers.executeQuery();
+
+        // If partial update was not used, LASTNAME would not be 'modified'
+        customer = root.getDataObject("CUSTOMER[1]");
+        assertEquals(1, customer.getInt("ID"));
+        assertEquals("lastname modified", customer.getString("LASTNAME"));
+        assertEquals("address modified", customer.getString("ADDRESS"));
+    }    
+    
+    /**
+     * Insert with no changed properties and no generated ID
+     * Read graph.  Add new DO and apply without seting any attributes.
+     * 
+     */
+//    public void testPartialReadInsertApply() throws Exception {
+//
+//        DAS das = DAS.FACTORY.createDAS(getConnection());
+//        // Read some customers
+//        Command select = das.createCommand("Select * from CUSTOMER");
+//        DataObject root = select.executeQuery();
+//        
+//        //Remember count
+//        int count = root.getList("CUSTOMER").size();
+//
+//        // Create a new customer
+//        root.createDataObject("CUSTOMER");
+//
+//        // Build apply changes command
+//        das.applyChanges(root);
+//
+//        // verify insert
+//        root = select.executeQuery();
+//        assertEquals(count + 1, root.getList("CUSTOMER").size());
+//
+//    }
+
+    /**
+     * Insert with no changed properties to row with generated ID
+     * Read graph.  Add new DO and apply without seting any attributes. 
+     */
+    public void testPartialInsertWithGeneratedID() throws Exception {
+
+        DAS das = DAS.FACTORY.createDAS(getConfig("CompanyConfig.xml"),getConnection());
+        // Read some companies
+        Command select = das.createCommand("Select * from COMPANY");
+        DataObject root = select.executeQuery();
+        
+        //Remember count
+        int count = root.getList("COMPANY").size();
+
+        // Create a new company
+        root.createDataObject("COMPANY");
+
+        // Build apply changes command
+        das.applyChanges(root);
+
+        // verify insert
+        root = select.executeQuery();
+        assertEquals(count + 1, root.getList("COMPANY").size());
+
+    }
+    
+    /**
+     * Insert with 1 changed property that is not the ID
+     * @throws SQLException
+     */
+    public void testPartialInsertWithSinglePropertyChange() throws Exception {
+
+        DAS das = DAS.FACTORY.createDAS(getConfig("CompanyConfig.xml"),getConnection());
+        // Read some company
+        Command select = das.createCommand("Select * from COMPANY");
+        DataObject root = select.executeQuery();
+        
+        //Remember count
+        int count = root.getList("COMPANY").size();
+
+        // Create a new company
+        DataObject company = root.createDataObject("COMPANY");
+        company.setString("NAME", "New company name");
+        
+        // Build apply changes command
+        das.applyChanges(root);
+
+        // verify insert
+        root = select.executeQuery();
+        assertEquals(count + 1, root.getList("COMPANY").size());
+    }
+
+    /**
+     * Insert with 1 changed property that is the ID
+     * @throws SQLException
+     */
+    public void testPartialInsertWithIdChange() throws SQLException {
+        DAS das = DAS.FACTORY.createDAS(getConnection());
+        Command readCustomers = das.createCommand("select * from CUSTOMER");
+
+        // Read
+        DataObject root = readCustomers.executeQuery();
+
+        // Create a new customer
+        DataObject newCust = root.createDataObject("CUSTOMER");
+        newCust.set("ID", Integer.valueOf(200));
+        // Purposely do not set lastname to let it default to 'Garfugengheist'
+        // newCust.set("LASTNAME", "Gerkin" );
+
+        das.applyChanges(root);
+
+        Command readNewCust = das.createCommand("select * from CUSTOMER where ID = 200");
+        root = readNewCust.executeQuery();
+
+        // If partial insert was not used, LASTNAME would not be
+        // 'Garfugengheist'
+        newCust = root.getDataObject("CUSTOMER[1]");
+        assertEquals(200, newCust.getInt("ID"));
+        assertEquals("Garfugengheist", newCust.getString("LASTNAME"));
+        assertNull(newCust.getString("ADDRESS"));
+
+    }
+    
+    public void testPartialInsertWithAllPropertyChange() throws SQLException {
+        DAS das = DAS.FACTORY.createDAS(getConnection());
+        Command readCustomers = das.createCommand("select * from CUSTOMER");
+
+        // Read
+        DataObject root = readCustomers.executeQuery();
+
+        // Create a new customer
+        DataObject newCust = root.createDataObject("CUSTOMER");
+        newCust.set("ID", Integer.valueOf(300));
+        newCust.set("ADDRESS", "5528 Wells Fargo Drive");
+        newCust.set("LASTNAME", "Gerkin" );
+
+        das.applyChanges(root);
+
+        Command readNewCust = das.createCommand("select * from CUSTOMER where ID = 300");
+        root = readNewCust.executeQuery();
+
+        newCust = root.getDataObject("CUSTOMER[1]");
+        assertEquals(300, newCust.getInt("ID"));
+        assertEquals("Gerkin", newCust.getString("LASTNAME"));
+        assertEquals("5528 Wells Fargo Drive", newCust.getString("ADDRESS"));
+
+    }
+    
+    public void testPartialInsert() throws SQLException {
+        DAS das = DAS.FACTORY.createDAS(getConnection());
+        Command readCustomers = das.createCommand("select * from CUSTOMER");
 
         // Read
         DataObject root = readCustomers.executeQuery();
@@ -96,4 +262,6 @@ public class PartialUpdateTests extends DasTest {
         assertEquals("5528 Wells Fargo Drive", newCust.getString("ADDRESS"));
 
     }
+    
+    
 }
