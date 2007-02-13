59770e85ec0cc6678b8d025d6e0f7cd46d21c5b4
Luciano Resende <lresende@apache.org>
2007-02-13 05:15:00 +0000

    Removing @author tags
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/das@506861 13f79535-47bb-0310-9956-ffa450edef68

--- rdb/src/main/java/org/apache/tuscany/das/rdb/util/LoggerFactory.java
+++ rdb/src/main/java/org/apache/tuscany/das/rdb/util/LoggerFactory.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.tuscany.das.rdb.util;
@@ -30,8 +30,6 @@ import org.apache.log4j.PatternLayout;
 /**
  * Helper class for logging using external log utility : log4j
  * This will use log4j and to configure it, please add/update log4j.properties
- * 
- * @author lresende
  */
 
 public final class LoggerFactory {
--- rdb/src/main/java/org/apache/tuscany/das/rdb/util/LoggerLayout.java
+++ rdb/src/main/java/org/apache/tuscany/das/rdb/util/LoggerLayout.java
@@ -6,21 +6,20 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 package org.apache.tuscany.das.rdb.util;
 
 /**
  * Log4J layout helper for Tuscany DAS
- * @author lresende
  */
 public final class LoggerLayout {
 
@@ -45,7 +44,7 @@ public final class LoggerLayout {
          *  %t Threadname
          *  %x, %X see Doku
          *  %% individual percentage sign
-         *  
+         *
          *  Caution: %C, %F, %l, %L, %M slow down program run!
          *  Note, %n is newline
          */
