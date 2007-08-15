e8dd31835670510935f5310c70624ebb2c0fa7fd
Luciano Resende <lresende@apache.org>
2007-08-15 15:54:37 +0000

    TUSCANY-1538 - Applying patch from Ole Ersoy
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/das@566232 13f79535-47bb-0310-9956-ffa450edef68

--- ldap/das.ldap/src/test/java/org/apache/tuscany/das/ldap/create/test/InitialContextCreatorTest.java
+++ ldap/das.ldap/src/test/java/org/apache/tuscany/das/ldap/create/test/InitialContextCreatorTest.java
@@ -6,16 +6,16 @@
  *  to you under the Apache License, Version 2.0 (the
  *  "License"); you may not use this file except in compliance
  *  with the License.  You may obtain a copy of the License at
- *  
+ *
  *    http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing,
  *  software distributed under the License is distributed on an
  *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
- *  under the License. 
- *  
+ *  under the License.
+ *
  */
 package org.apache.tuscany.das.ldap.create.test;
 
@@ -25,7 +25,7 @@ import javax.naming.ldap.LdapContext;
 import org.apache.tuscany.das.ldap.create.InitialContextCreator;
 import org.apache.tuscany.das.ldap.schema.embedded.setup.test.AbstractTestSetup;
 
-public class InitialContextCreatorTest 
+public class InitialContextCreatorTest
 extends AbstractTestSetup
 {
     public void testCreate() throws NamingException
@@ -34,19 +34,25 @@ extends AbstractTestSetup
         LdapContext exampleContext  = null;
         LdapContext usersContext    = null;
         LdapContext accountsContext = null;
-                
-        InitialContextCreator. create( 
-            xsdNamespace, 
+        LdapContext rootContext     = null;
+
+        rootContext                 =
+            InitialContextCreator.create(
+            xsdNamespace,
             dasPartitionContext );
-        
-        comContext      = 
-            ( LdapContext ) 
+
+        assertEquals(
+            rootContext.getNameInNamespace(),
+            "cn=accounts,cn=users,cn=example,cn=com,ou=das");
+
+        comContext      =
+            ( LdapContext )
             dasPartitionContext.lookup( "cn=com" );
         exampleContext  = ( LdapContext )comContext.lookup( "cn=example" );
         usersContext    = ( LdapContext ) exampleContext.lookup( "cn=users" );
         accountsContext = ( LdapContext ) usersContext.lookup( "cn=accounts" );
         accountsContext = ( LdapContext ) usersContext.lookup( "cn=accounts" );
-        
+
         assertNotNull(comContext);
         assertNotNull(exampleContext);
         assertNotNull(usersContext);
@@ -54,7 +60,7 @@ extends AbstractTestSetup
 
         usersContext.
         destroySubcontext( "cn=accounts" );
-        
+
         exampleContext.
         destroySubcontext( "cn=users" );
 
